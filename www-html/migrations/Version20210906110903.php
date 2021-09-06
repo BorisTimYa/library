0980860350208cc281214320d58903a4bdb39f07
Boris Tim <boris.tim@gmail.com>
2021-09-06 16:30:33 +0500

    Init

--- www-html/migrations/Version20210906110903.php
+++ www-html/migrations/Version20210906110903.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210906103728 extends AbstractMigration
+final class Version20210906110903 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,15 +20,18 @@ final class Version20210906103728 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SEQUENCE author_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE SEQUENCE book_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE author (id INT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE TABLE book (id INT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE author (id SERIAL NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE book (id SERIAL NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE TABLE book_author (book_id INT NOT NULL, author_id INT NOT NULL, PRIMARY KEY(book_id, author_id))');
         $this->addSql('CREATE INDEX IDX_9478D34516A2B381 ON book_author (book_id)');
         $this->addSql('CREATE INDEX IDX_9478D345F675F31B ON book_author (author_id)');
         $this->addSql('ALTER TABLE book_author ADD CONSTRAINT FK_9478D34516A2B381 FOREIGN KEY (book_id) REFERENCES book (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE book_author ADD CONSTRAINT FK_9478D345F675F31B FOREIGN KEY (author_id) REFERENCES author (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+
+        // Generate demo data
+        $this->addSql("insert into author(name) select 'Author#'||' '||i::text from generate_series(1, 10000) s(i);");
+        $this->addSql("insert into book(name) select 'Book#'||' '||i::text from generate_series(1, 10000) s(i);");
+        $this->addSql("insert into book_author(book_id,author_id) select i::int,(10001 - i::int) from generate_series(1, 10000) s(i);");
     }
 
     public function down(Schema $schema): void
@@ -37,8 +40,6 @@ final class Version20210906103728 extends AbstractMigration
         $this->addSql('CREATE SCHEMA public');
         $this->addSql('ALTER TABLE book_author DROP CONSTRAINT FK_9478D345F675F31B');
         $this->addSql('ALTER TABLE book_author DROP CONSTRAINT FK_9478D34516A2B381');
-        $this->addSql('DROP SEQUENCE author_id_seq CASCADE');
-        $this->addSql('DROP SEQUENCE book_id_seq CASCADE');
         $this->addSql('DROP TABLE author');
         $this->addSql('DROP TABLE book');
         $this->addSql('DROP TABLE book_author');
