3cb3e4c5766cc383ab3842b26b76c21588d2e524
Boris Tim <boris.tim@gmail.com>
2021-09-07 00:38:24 +0500

    Init

--- www-html/src/Controller/AuthorController.php
+++ www-html/src/Controller/AuthorController.php
@@ -2,20 +2,55 @@
 
 namespace App\Controller;
 
+
+use App\Repository\AuthorRepository;
+use Doctrine\ORM\EntityManagerInterface as EntityManagerInterface;
+use Monolog\Logger;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Author;
 
+/**
+ * Class AuthorController
+ *
+ * @package App\Controller
+ * @Route("/author")
+ */
 class AuthorController extends AbstractController
 {
+
     /**
-     * @Route("/author", name="author")
+     * @Route("/{id}", name="author", requirements={"id"="\d+"}, methods={"GET"})
      */
-    public function index(): Response
+    public function index(int $id, AuthorRepository $authorRepository): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/AuthorController.php',
-        ]);
+        if ($author = $authorRepository->find($id)) {
+            return new JsonResponse(['name' => $author->getName(),]);
+        }
+        throw $this->createNotFoundException("Author $id not found");
     }
+
+    /**
+     * @Route("/create", name="author_create", methods={"PUT"})
+     */
+    public function create(Request $request, AuthorRepository $authorRepository, EntityManagerInterface $entityManager,LoggerInterface $logger): JsonResponse
+    {
+        if (($json = $request->getContent()) && ($json = json_decode($json)) && (isset($json['name']))) {
+            if (!$author = $authorRepository->findOneBy(['Name' => $json['name']])) {
+                $author = new Author();
+                $author->setName($json['name']);
+                $entityManager->persist($author);
+                $entityManager->flush();
+            }
+
+            return new JsonResponse(['id' => $author->getId(),]);
+        }
+        $logger->debug($request->getContent());
+        throw $this->createNotFoundException("No required data");
+    }
+
 }
--- www-html/src/Controller/BookController.php
+++ www-html/src/Controller/BookController.php
@@ -2,20 +2,51 @@
 
 namespace App\Controller;
 
+use App\Repository\BookRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class BookController
+ *
+ * @package App\Controller
+ * @Route("/book")
+ */
 class BookController extends AbstractController
 {
+
     /**
-     * @Route("/book", name="book")
+     * @param int $id
+     * @param \App\Repository\BookRepository $bookRepository
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/book/{id}", name="book", requirements={"id"="\d+"}, methods={"GET"})
      */
-    public function index(): Response
+    public function index(int $id, BookRepository $bookRepository): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/BookController.php',
-        ]);
+        if ($book = $bookRepository->find($id)) {
+            return new JsonResponse(['name' => $book->getName()]);
+        }
+        $this->createNotFoundException("Book $id not found");
     }
+
+    /**
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param \App\Repository\BookRepository $bookRepository
+     * @param \Doctrine\ORM\EntityManagerInterface $entityManager
+     * @param \Psr\Log\LoggerInterface $logger
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/create", name="book_create", methods={"PUT"})
+     */
+    public function create(Request $request, BookRepository $bookRepository, EntityManagerInterface $entityManager, LoggerInterface $logger): Response
+    {
+
+    }
+
 }
